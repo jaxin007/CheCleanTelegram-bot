71168b2bce8d9c57c2c33801154b2429edc598a9
Dima Bilozor <swatdimonbil@gmail.com>
2020-05-10 16:46:41 +0300

    CC-50 changed indentation

--- src/api-service.js
+++ src/api-service.js
@@ -2,15 +2,15 @@ import axios from 'axios';
 import * as path from 'path';
 
 export class ApiService {
-    constructor(apiUrl) {
-        this.apiUrl = apiUrl;
-    }
+  constructor(apiUrl) {
+    this.apiUrl = apiUrl;
+  }
 
-    sendCase(createdCase) {
-        return axios.post(path.join(this.apiUrl, 'case'), createdCase, {
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        });
-    }
+  sendCase(createdCase) {
+    return axios.post(path.join(this.apiUrl, 'case'), createdCase, {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
 }
