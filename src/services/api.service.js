8e1ad09b57801b3baa061dfb310a3cedd5438ee7
Dima Bilozor <swatdimonbil@gmail.com>
2020-06-25 10:56:33 +0300

    СС-80 changed photo storage to GCS

--- src/services/api.service.js
+++ src/services/api.service.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
+const { Storage } = require('@google-cloud/storage');
+const path = require('path');
 
 class ApiService {
   constructor(apiUrl) {
     this.apiUrl = apiUrl;
+    this.gc = new Storage({
+      keyFilename: path.join(__dirname, `../api/${process.env.GOOGLE_PROJECT_ACCOUNT_FILE}`),
+      projectId: process.env.GOOGLE_PROJECT_ID,
+    });
   }
 
   sendCase(createdCase) {
@@ -13,6 +19,17 @@ class ApiService {
     }).catch((err) => {
       throw new Error(err);
     });
+  }
+
+  async uploadFile(url, fileName) {
+    const response = (await (axios.get(url, { responseType: 'arraybuffer' }))).data;
+    this.gc.bucket(process.env.GOOGLE_BUCKET_NAME)
+      .file(fileName)
+      .save(response, (err) => {
+        if (err) {
+          throw new Error(err);
+        }
+      });
   }
 }
 
